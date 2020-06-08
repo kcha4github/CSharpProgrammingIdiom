1975490492fdca830150c266cbcb3376ae0c0029
Kunio Tsuge <kunio.tsuge@gmail.com>
2020-06-09 08:06:50 +0900

    最初のバージョン。ベタな算出方法。

--- chapter2_1/Program.cs
+++ chapter2_1/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace chapter2_1
+namespace DistanceConverter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // フィートからメートルへの対応表を出力
+            for(int feet = 1; feet <= 10; feet++)
+            {
+                double meter = feet * 0.3048;
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
         }
     }
 }
