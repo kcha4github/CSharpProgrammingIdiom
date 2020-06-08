986a53951ea33f329ffffe6af6bb3e01b0a26523
Kunio Tsuge <kunio.tsuge@gmail.com>
2020-06-09 08:09:46 +0900

    距離計算をメソッドとして独立させる

--- chapter2_1/Program.cs
+++ chapter2_1/Program.cs
@@ -9,9 +9,19 @@ namespace DistanceConverter
             // フィートからメートルへの対応表を出力
             for(int feet = 1; feet <= 10; feet++)
             {
-                double meter = feet * 0.3048;
+                double meter = FeetToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
+        }
+
+        /// <summary>
+        /// フィートからメートルを求める
+        /// </summary>
+        /// <param name="feet"></param>
+        /// <returns></returns>
+        static double FeetToMeter(int feet)
+        {
+            return feet * 0.3048;
         }
     }
 }
