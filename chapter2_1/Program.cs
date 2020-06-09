8e4f82acf9c6837d36c6f0a95fb9e872718175cc
Kunio Tsuge <kunio.tsuge@gmail.com>
2020-06-09 09:13:23 +0900

    メートルからフィートへの対応表を追加。書籍のコマンドラインパラメータ分岐は実装しない

--- chapter2_1/Program.cs
+++ chapter2_1/Program.cs
@@ -12,6 +12,13 @@ namespace DistanceConverter
                 double meter = FeetToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
+
+            // メートルからフィートへの対応表を出力
+            for(int meter = 1; meter <= 10; meter++)
+            {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+            }
         }
 
         /// <summary>
@@ -22,6 +29,11 @@ namespace DistanceConverter
         static double FeetToMeter(int feet)
         {
             return feet * 0.3048;
+        }
+
+        static double MeterToFeet(int meter)
+        {
+            return meter / 0.3048;
         }
     }
 }
