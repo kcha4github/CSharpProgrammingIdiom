3fb4b630b4c406e1832be1708cc4d3bdac1736a2
Kunio Tsuge <kunio.tsuge@gmail.com>
2020-06-10 21:00:57 +0900

    2.2.1 Saleクラスを定義する

---
+++ chapter2_2/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chapter2_2
+{
+    public class Sale
+    {
+        //店舗名
+        public string ShopName { get; set; }
+        //商品カテゴリ
+        public string ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
+    }
+}
