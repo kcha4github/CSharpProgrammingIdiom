4b2909d2cf092839d14e1c863e960b19ab4a8799
Kunio Tsuge <kunio.tsuge@gmail.com>
2020-06-10 21:20:48 +0900

    2.2.3 店舗別売上を求める

---
+++ chapter2_2/SalesCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chapter2_2
+{
+    /// <summary>
+    /// 売上集計クラス
+    /// </summary>
+    public class SalesCounter
+    {
+        private List<Sale> _sales;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="sales"></param>
+        public SalesCounter(List<Sale> sales)
+        {
+            _sales = sales;
+        }
+
+        /// <summary>
+        /// 店舗別売上を求める
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, int> GetPerStoreSales()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach(Sale sale in _sales)
+            {
+                if (dict.ContainsKey(sale.ShopName))
+                {
+                    dict[sale.ShopName] += sale.Amount;
+                }
+                else
+                {
+                    dict[sale.ShopName] = sale.Amount;
+                }
+            }
+            return dict;
+        }
+    }
+}
