408bcc38e76269edfc5158c81c70c0cb6bc977be
Kunio Tsuge <kunio.tsuge@gmail.com>
2020-06-10 21:15:00 +0900

    2.2.2 CSVファイルを読み込む

--- chapter2_2/Program.cs
+++ chapter2_2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace chapter2_2
 {
@@ -7,6 +9,31 @@ namespace chapter2_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 売上データを読み込み、Saleオブジェクトのリストを返す
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns></returns>
+        static List<Sale> ReadSales(string filepath)
+        {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filepath);
+
+            foreach(string line in lines)
+            {
+                string[] items = line.Split(',');
+                Sale sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+
+            return sales;
         }
     }
 }
